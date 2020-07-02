471cf41d87cfb07cf0841bb74c26e986026577c3
rhjoerg <rhjoerg@gmail.com>
2020-07-02 07:23:22 +0200

    PlexusExtension working as expected

--- rhjoerg-plexus-starter-test/src/main/java/ch/rhjoerg/plexus/starter/test/PlexusExtension.java
+++ rhjoerg-plexus-starter-test/src/main/java/ch/rhjoerg/plexus/starter/test/PlexusExtension.java
@@ -1,45 +1,106 @@
 package ch.rhjoerg.plexus.starter.test;
 
-import static ch.rhjoerg.commons.annotation.Annotations.findAnnotationDetails;
+import static ch.rhjoerg.commons.reflect.Classes.walkClassTree;
+import static ch.rhjoerg.plexus.starter.container.ContainerUtils.plexusStarterContainer;
 
-import java.util.List;
-import java.util.Map;
-
+import org.codehaus.plexus.DefaultPlexusContainer;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
 
+import com.google.inject.Injector;
+
+import ch.rhjoerg.commons.reflect.ClassVisitor;
 import ch.rhjoerg.plexus.starter.PlexusConfigurations;
 
 public class PlexusExtension implements BeforeAllCallback, BeforeEachCallback, AfterAllCallback
 {
-	@Override
-	public void beforeEach(ExtensionContext context) throws Exception
-	{
-		discoverConfigurationClass(context);
-	}
+	public static Namespace NAMESPACE = Namespace.create(PlexusExtension.class);
+	public static Object CONTAINER_KEY = DefaultPlexusContainer.class;
+	public static Object INJECTOR_KEY = Injector.class;
 
 	@Override
 	public void beforeAll(ExtensionContext context) throws Exception
 	{
+		Class<?> configurationClass = discoverConfigurationClass(context);
+		DefaultPlexusContainer container = plexusStarterContainer(configurationClass);
+		Store store = context.getStore(NAMESPACE);
+
+		store.put(CONTAINER_KEY, container);
+		store.put(INJECTOR_KEY, container.lookup(Injector.class));
 	}
 
 	@Override
 	public void afterAll(ExtensionContext context) throws Exception
 	{
+		Store store = context.getStore(NAMESPACE);
+		DefaultPlexusContainer container = store.get(CONTAINER_KEY, DefaultPlexusContainer.class);
+
+		store.remove(INJECTOR_KEY);
+		store.remove(CONTAINER_KEY);
+
+		if (container != null)
+		{
+			container.dispose();
+		}
+	}
+
+	@Override
+	public void beforeEach(ExtensionContext context) throws Exception
+	{
+		Injector injector = context.getStore(NAMESPACE).get(INJECTOR_KEY, Injector.class);
+
+		injector.injectMembers(context.getRequiredTestInstance());
 	}
 
 	private Class<?> discoverConfigurationClass(ExtensionContext context)
 	{
 		Class<?> testClass = context.getRequiredTestClass();
-		List<Map.Entry<Class<?>, List<PlexusConfigurations>>> candidates = findAnnotationDetails(PlexusConfigurations.class, testClass);
+		DiscoverConfigurationClassVisitor visitor = new DiscoverConfigurationClassVisitor();
 
-		if (candidates.isEmpty())
+		walkClassTree(testClass, visitor);
+
+		return visitor.result == null ? testClass : visitor.result;
+	}
+
+	public static class DiscoverConfigurationClassVisitor implements ClassVisitor
+	{
+		public Class<?> result;
+
+		@Override
+		public boolean enterClass(Class<?> type)
 		{
-			return testClass;
+			return result == null;
 		}
 
-		return candidates.get(0).getKey();
+		@Override
+		public boolean leaveClass(Class<?> type)
+		{
+			return result == null;
+		}
+
+		@Override
+		public boolean visitClass(Class<?> type)
+		{
+			if (hasRequiredAnnotation(type))
+			{
+				result = type;
+			}
+
+			return result == null;
+		}
+
+		private boolean hasRequiredAnnotation(Class<?> type)
+		{
+			if (type.isAnnotationPresent(PlexusConfigurations.class))
+			{
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
--- rhjoerg-plexus-starter-test/src/test/java/ch/rhjoerg/plexus/starter/test/ExtendWithTests.java
+++ rhjoerg-plexus-starter-test/src/test/java/ch/rhjoerg/plexus/starter/test/ExtendWithTests.java
@@ -1,14 +1,23 @@
 package ch.rhjoerg.plexus.starter.test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.google.inject.Injector;
 
 @ExtendWith(PlexusExtension.class)
 public class ExtendWithTests
 {
+	@Inject
+	private Injector injector;
+
 	@Test
 	public void test()
 	{
-		System.err.println("NOT YET IMPLEMENTED: ExtendWithTests");
+		assertNotNull(injector);
 	}
 }
